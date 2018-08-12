6f1cc14462b57f7001a3876403807a722c3b27ef
Jimit Raithatha <princejim87@Jimits-MacBook-Air.local>
2018-08-12 16:18:27 -0400

    Parameterized eta and sns_target_arn

--- eta_to_work.py
+++ eta_to_work.py
@@ -8,18 +8,19 @@ def lambda_handler(event, context):
     going_to = os.environ['going_to']
     google_api_key = os.environ['google_api_key']
     google_directions_url = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + start_from.replace(' ', '+') + '&destination=' + going_to.replace(' ', '+') + '4&key=' + google_api_key
+    sns_target_arn = os.environ['sns_target_arn']  # whom to inform if ETA is more than desired
+    desired_eta_in_mins = os.environ['desired_eta_in_mins']
     html_request = urllib2.Request(google_directions_url)
     response = urllib2.urlopen(html_request)
     response_text = json.loads(response.read())
     eta_in_mins = (response_text['routes'][0]['legs'][0]['duration']['text']).split(' ')[0]
-    print(eta_in_mins)
 
     #Send SNS message to me if ETA to work is greater than 30 minutes
-    if eta_in_mins > 30:
+    if int(eta_in_mins) > int(desired_eta_in_mins):
         message = "ETA to " + going_to + " is " + eta_in_mins + " minutes. Leave early"
         sns_client = boto3.client('sns', region_name='us-east-1')
         sns_response = sns_client.publish(
-            TargetArn='arn:aws:sns:us-east-1:<aws_account_#>:jimit_eta_to_work',
+            TargetArn=sns_target_arn,
             Message=message,
             MessageStructure='text'
         )
